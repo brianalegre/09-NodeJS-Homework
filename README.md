27e2d21226c0cfa6fac41eb2c232cd6eda4d71f8
Brian Alegre <brialegre@yahoo.com>
2022-05-15 18:34:06 -0700

    Created INQ questions.  Able to create JSON from answers

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # 09-NodeJS-Homework
+
+# Site Preview
+
+<p align="center">
+    <img alt="Site Preview" src="./assets/images/SitePreview.png">
+</p>
+
+# Github Links
+
+-   Repository:
+-   Deployed:
+
+# Criteria
+
+-   Inquirer
+-   CLI accepts user input
+-   User Inputs gets created with:
+    -   Title of Project
+        -   Text Input
+    -   Table of Contents
+        -   Clickable links to each section
+    -   Installation
+        -   Text Input
+    -   Usage
+        -   Text Input
+    -   License
+        -   List of Options
+        -   A badge for that license is added near the to of the README
+    -   Contributing
+        -   Text Input
+    -   Tests
+        -   Text Input
+    -   Questions
+        -   Text Input
+        -   Enter Github Username
+        -   Enter Email
+
+# Notes and Articles
---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+// Node Variables
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { windowCount } = require('rxjs');
+
+// CLI Prompts
+inquirer
+	.prompt([
+		{
+			type: 'input',
+			// File Name
+			name: 'project',
+			message: 'What is your project name?',
+			default: 'Node Project',
+		},
+		{
+			type: 'input',
+			message: 'How do I install your application?',
+			name: 'install',
+			default: 'Copy Pasta',
+		},
+		{
+			type: 'input',
+			message: 'What is your application used for?',
+			name: 'usage',
+			default: 'Gaining EXP with Node',
+		},
+		{
+			type: 'checkbox',
+			message: 'What kind of license is used?',
+			name: 'license',
+			choices: ['1', '2', '3', '4'],
+			default: ['3'],
+		},
+		{
+			type: 'input',
+			message: 'How do I contribute?',
+			name: 'contribute',
+			default: 'You can NOT',
+		},
+		{
+			type: 'input',
+			message: 'How do I test your application?',
+			name: 'test',
+			default: 'Play with it',
+		},
+		{
+			type: 'input',
+			message: 'What is your Github Username?',
+			name: 'github',
+			default: 'BrianAlegre',
+		},
+		{
+			type: 'input',
+			message: 'Please enter in your email',
+			name: 'email',
+			default: 'brialegre@yahoo.com',
+		},
+	])
+	.then((data) => {
+		const filename = `${data.project
+			.toLowerCase()
+			.split(' ')
+			.join('')}.json`;
+
+		fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+			err ? console.log(err) : console.log('Success!')
+		);
+	});
